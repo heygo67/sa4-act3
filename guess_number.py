b551afe66f61ace4c5d9d6c3ec80bca419162131
McKenna Noakes <mckenna.noakes@outlook.com>
2024-02-08 01:25:34 -0600

    added a cap to the guesses at 5. ends the game if user goes over 5 guesses

--- guess_number.py
+++ guess_number.py
@@ -1,16 +1,22 @@
 number = 10
+guesses = 5
 
 print("I'm thinking of a number...")
 guess = int(input("What number am I thinking of? (enter \"q\" to quit.) "))
 
-while guess != "q":
-    if guess == number:
-        print("Congratulations! You guessed the right number.")
-        break
-    else:
-        guess = input(f"Sorry! Try again! (enter \"q\" to quit.) ")
-        if guess == "q":
-            print(f'The correct number was {number}. Thanks for playing!')
+while guesses != 0:
+    while guess != "q":
+        if guess == number:
+            print("Congratulations! You guessed the right number.")
             break
+        else:
+            guesses += -1
+            if guesses == 0:
+                print(f'Ran out of guesses! The correct number was {number}')
+                break
+            guess = input(f"Sorry! Try again! {guesses} guess(es) left! (enter \"q\" to quit.) ")
+            if guess == "q":
+                print(f'The correct number was {number}. Thanks for playing!')
+                break
 
 

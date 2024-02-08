d91f7a4efc568b8335f1cc0d1e6fb220f3ccead1
McKenna Noakes <mckenna.noakes@outlook.com>
2024-02-08 01:09:50 -0600

    added an option to quit. game repeats when incorrect until correct key is entered.

--- guess_number.py
+++ guess_number.py
@@ -1,10 +1,16 @@
 number = 10
 
 print("I'm thinking of a number...")
-guess = int(input("What number am I thinking of? "))
+guess = int(input("What number am I thinking of? (enter \"q\" to quit.) "))
 
-if guess == number:
-    print("Congratulations! You guessed the right number.")
-else:
-    print(f"Sorry! The number was {number}.")
+while guess != "q":
+    if guess == number:
+        print("Congratulations! You guessed the right number.")
+        break
+    else:
+        guess = input(f"Sorry! Try again! (enter \"q\" to quit.) ")
+        if guess == "q":
+            print(f'The correct number was {number}. Thanks for playing!')
+            break
+
 
